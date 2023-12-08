996656f40b6b48291acabee9db3e6f3e1e8e1a40
Ho Phuc Thai <hophucthai9@gmail.com>
2023-12-08 21:54:44 +0700

    how to get by id in rest

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -4,7 +4,9 @@ import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,17 +15,18 @@ import java.util.UUID;
 @AllArgsConstructor
 @RestController
 @Slf4j
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeer(){
         return beerService.listBeer();
     }
 
-
-    public Beer getBeerById(UUID id){
+    @RequestMapping( value = "/{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId){
         log.debug("Get Beer by Id - in controller");
-        return beerService.getBearById(id);
+        return beerService.getBearById(beerId);
     }
 }

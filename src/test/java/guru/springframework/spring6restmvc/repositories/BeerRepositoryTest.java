3907cddddbe264dcfe7a7a92eb2f8e2fd760844d
Ho Phuc Thai <hophucthai9@gmail.com>
2023-12-18 01:22:15 +0700

    create repository test

---
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.spring6restmvc.repositories;
+
+import guru.springframework.spring6restmvc.entities.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class BeerRepositoryTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testSaveBeer(){
+        Beer beer = beerRepository.save(Beer.builder()
+                .beerName("New Name")
+                .build());
+
+        assertThat(beer).isNotNull();
+        assertThat(beer.getId()).isNotNull();
+    }
+}

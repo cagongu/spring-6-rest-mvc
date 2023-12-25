f3296b14ac370cecac41dcf0d244eba15b7bb284
Ho Phuc Thai <hophucthai9@gmail.com>
2023-12-25 22:53:20 +0700

    test too long beer name

--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,12 +10,29 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testSaveBeerToLong() {
+        assertThrows(ConstraintViolationException.class, () -> {
+            Beer beer = beerRepository.save(Beer.builder()
+                    .beerName("New Name sadsadsadsadsadddddddddddddddddddddddddddddddddddddddddddddddd")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("12312321321")
+                    .price(new BigDecimal("11.99"))
+                    .build());
+
+            beerRepository.flush();
+            assertThat(beer).isNotNull();
+            assertThat(beer.getId()).isNotNull();
+        });
+    }
 
     @Test
     void testSaveBeer() {

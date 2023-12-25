f15c3ca9d83f341115cca1ef3652dd8fb5e1bb6c
Ho Phuc Thai <hophucthai9@gmail.com>
2023-12-25 22:35:05 +0700

    add validate full beer

--- src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,10 +17,15 @@ class BeerRepositoryTest {
     BeerRepository beerRepository;
 
     @Test
-    void testSaveBeer(){
+    void testSaveBeer() {
         Beer beer = beerRepository.save(Beer.builder()
                 .beerName("New Name")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("12312321321")
+                .price(new BigDecimal("11.99"))
                 .build());
+
+        beerRepository.flush();
 
         assertThat(beer).isNotNull();
         assertThat(beer.getId()).isNotNull();

78616ed6a3f97abba97eef7f3e148a6091a0856d
cagongu <hophucthai9@gmail.com>
2024-08-10 23:18:21 +0700

    add executions in pom

--- target/generated-sources/annotations/guru/springframework/spring6restmvc/mappers/CustomerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/spring6restmvc/mappers/CustomerMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-10T23:11:03+0700",
+    date = "2024-08-10T23:15:58+0700",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.9 (Oracle Corporation)"
 )
 @Component

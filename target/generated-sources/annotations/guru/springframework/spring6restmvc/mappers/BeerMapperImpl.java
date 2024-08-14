f4d6ffa19c5c684e1eb5bb387a664376773ec063
cagongu <hophucthai9@gmail.com>
2024-08-14 15:04:06 +0700

    check

--- target/generated-sources/annotations/guru/springframework/spring6restmvc/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/spring6restmvc/mappers/BeerMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-10T23:15:59+0700",
+    date = "2024-08-10T23:54:33+0700",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.9 (Oracle Corporation)"
 )
 @Component

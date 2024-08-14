b6b939f36bfd8eadc814791e1be01fc0b06df949
cagongu <hophucthai9@gmail.com>
2024-08-14 16:22:49 +0700

    add sec cfg

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -12,9 +12,13 @@ public class SpringSecConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth->oauth.jwt(Customizer.withDefaults()));
+        http.authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/v3/api-docs**", "/swagger-ui/**",  "/swagger-ui.html").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> {
+                    httpSecurityOAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults());
+                });
 
         return http.build();
     }
--- src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -40,7 +40,7 @@ public class Beer {
     private String beerName;
 
     @NotNull
-    @JdbcTypeCode(value = SqlTypes.SMALLINT)
+//    @JdbcTypeCode(value = SqlTypes.SMALLINT)
     private BeerStyle beerStyle;
 
     @NotNull

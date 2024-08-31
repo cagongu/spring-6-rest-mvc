5dd32f825e06eb30448168ee8e82cd163fb810f1
cagongu <hophucthai9@gmail.com>
2024-08-31 13:32:45 +0700

    adding deps and Enable caching

--- src/main/java/guru/springframework/spring6restmvc/Spring6RestMvcApplication.java
+++ src/main/java/guru/springframework/spring6restmvc/Spring6RestMvcApplication.java
@@ -1,7 +1,9 @@
 package guru.springframework.spring6restmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class Spring6RestMvcApplication {
 

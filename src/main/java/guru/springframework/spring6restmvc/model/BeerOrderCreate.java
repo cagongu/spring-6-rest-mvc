cd7a15ffa43ec4c6baf051e1695a3bf3eb73e0e6
cagongu <hophucthai9@gmail.com>
2024-09-10 02:47:21 +0700

    adding DTO for create operation

---
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerOrderCreate.java
@@ -0,0 +1,19 @@
+package guru.springframework.spring6restmvc.model;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Builder
+public class BeerOrderCreate {
+    private String customerRef;
+
+    @NotNull
+    private UUID customerId;
+
+    private Set<BeerOrderLineCreateDTO> beerOrderLines;
+}

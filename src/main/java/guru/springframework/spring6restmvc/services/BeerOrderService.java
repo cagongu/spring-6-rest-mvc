0ae852a369c13e2506a02033c52e7d1905f7f021
cagongu <hophucthai9@gmail.com>
2024-09-10 02:41:43 +0700

    adding get operations

---
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerOrderService.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.BeerOrderDTO;
+import org.springframework.data.domain.Page;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BeerOrderService {
+
+    Optional<BeerOrderDTO> getById(UUID beerOrderId);
+
+    Page<BeerOrderDTO> listOrders(Integer pageNumber, Integer pageSize);
+}

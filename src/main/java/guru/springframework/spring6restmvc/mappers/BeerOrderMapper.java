809bd8d3c7066a10ffc0750b3d7fbef28cd494bb
cagongu <hophucthai9@gmail.com>
2024-09-09 22:53:18 +0700

    Adding Mappers

---
+++ src/main/java/guru/springframework/spring6restmvc/mappers/BeerOrderMapper.java
@@ -0,0 +1,27 @@
+package guru.springframework.spring6restmvc.mappers;
+
+import guru.springframework.spring6restmvc.entities.BeerOrder;
+import guru.springframework.spring6restmvc.entities.BeerOrderLine;
+import guru.springframework.spring6restmvc.entities.BeerOrderShipment;
+import guru.springframework.spring6restmvc.model.BeerOrderDTO;
+import guru.springframework.spring6restmvc.model.BeerOrderLineDTO;
+import guru.springframework.spring6restmvc.model.BeerOrderShipmentDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper
+public interface BeerOrderMapper {
+    @Mapping(target = "beerOrder", ignore = true)
+    BeerOrderShipment beerOrderShipmentDtoToBeerOrderShipment(BeerOrderShipmentDTO beerOrderShipmentDTO);
+
+    BeerOrderShipmentDTO beerOrderShipmentToBeerOrderShipmentDto(BeerOrderShipment beerOrderShipment);
+
+    @Mapping(target = "beerOrder", ignore = true)
+    BeerOrderLine beerOrderLineDtoToBeerOrderLine(BeerOrderLineDTO beerOrderLineDTO);
+
+    BeerOrderLineDTO beerOrderLineToBeerOrderLineDto(BeerOrderLine beerOrderLine);
+
+    BeerOrder beerOrderDtoToBeerOrder(BeerOrderDTO beerOrder);
+
+    BeerOrderDTO beerOrderToBeerOrderDto(BeerOrder beerOrder);
+}
